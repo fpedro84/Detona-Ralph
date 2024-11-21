d58a2198bc4bf1bd8e028c5f1abbe970ec973053
Fabrício Pedro <mfpbtu@gmail.com>
2024-11-21 17:32:11 -0300

    Atualizar o engine.js

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -37,7 +37,7 @@ function countDown(){
 }
 
 function playSound(audioName) {
-    let audio = new Audio('./src/audios/${audioName}.m4a');
+    let audio = new Audio("./src/audios/${audioName}.m4a");
     audio.volume = 0.2;
     audio.play();
 }
